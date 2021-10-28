a43b108b9c6053f01c5568b4d12ac9b79c0dbb5b
morgan4080 <morganmurungi@live.com>
2021-10-28 08:54:43 +0300

    pages

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -191,8 +191,15 @@ function launchSpecific(stepInput: number) {
                     <h3>Set user roles</h3>
                   </div>
                 </div>
-                <div class="flex flex-col mx-auto pt-8">
-
+                <div class="flex flex-col pt-4">
+                  <div class="flex">
+                    <div class="flex-initial flex space-x-2 items-center py-4 px-3 rounded-md ring-2 bg-gray-50 ring-blue-300 text-gray-700 text-base cursor-pointer">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                      </svg>
+                      <p>Attach to existing roles</p>
+                    </div>
+                  </div>
                 </div>
                 <div class="flex flex-row justify-between pt-8 border-b pb-1">
                   <div class="font-medium text-lg text-gray-700">
