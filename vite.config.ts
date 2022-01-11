75c02c815c898f7c3752c09227dbcddd492f1894
morgan4080 <morganmurungi@live.com>
2022-01-11 16:24:55 +0300

    baseflagpackage.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
