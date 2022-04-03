67b6458706587f5c723ead8b341133d1bdbc4e53
l3nux <lennyndungu@gmail.com>
2022-04-03 22:59:51 +0300

    roles page done

--- src/views/Roles/_id.vue
+++ src/views/Roles/_id.vue
@@ -2,58 +2,9 @@
   import { getRole } from '@/modules/all'
   import { useRoute } from "vue-router"
   import {ref} from "vue";
-  const posts = [
-    {
-      title: 'Boost your conversion rate',
-      href: '#',
-      category: { name: 'Article', href: '#', color: 'bg-indigo-100 text-indigo-800' },
-      description:
-          'Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.',
-      date: 'Mar 16, 2020',
-      datetime: '2020-03-16',
-      author: {
-        name: 'Paul York',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      readingTime: '6 min',
-    },
-    {
-      title: 'How to use search engine optimization to drive sales',
-      href: '#',
-      category: { name: 'Video', href: '#', color: 'bg-pink-100 text-pink-800' },
-      description:
-          'Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.',
-      date: 'Mar 10, 2020',
-      datetime: '2020-03-10',
-      author: {
-        name: 'Dessie Ryan',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      readingTime: '4 min',
-    },
-    {
-      title: 'Improve your customer experience',
-      href: '#',
-      category: { name: 'Case Study', href: '#', color: 'bg-green-100 text-green-800' },
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab iure iusto fugiat commodi sequi.',
-      date: 'Feb 12, 2020',
-      datetime: '2020-02-12',
-      author: {
-        name: 'Easer Collins',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-      },
-      readingTime: '11 min',
-    },
-  ]
   const route = useRoute()
 
-  const role = ref()
+  const role:any = ref()
   getRole(route.params.id).then((r) => {
     role.value = r
   })
