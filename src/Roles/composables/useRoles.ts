67768d681797a3cbaa64c8800911eeca2d26a12c
Dickson <kibedickson3@gmail.com>
2023-03-07 11:12:07 +0300

    show temporary password

--- src/Roles/composables/useRoles.ts
+++ src/Roles/composables/useRoles.ts
@@ -3,6 +3,7 @@ import type { Role } from "@/Roles/types";
 import { Pageables } from "@/types";
 import { useQueryParams } from "@/composables/useQueryParams";
 import { mapActions } from "@/modules/mapStore";
+import { useFetch } from "@vueuse/core";
 
 export const useRoles = () => {
   // TODO refactor this to composable
@@ -12,6 +13,7 @@ export const useRoles = () => {
   const userRoles = ref();
   const isLoading = ref(false);
   const error = ref<null | unknown>(null);
+  const errors = ref<null | unknown>(null);
   const pageables = reactive({
     recordsPerPage: 10,
     totalRecords: 0,
@@ -24,42 +26,25 @@ export const useRoles = () => {
 
   async function fetchRoles() {
     isLoading.value = true;
-    error.value = null;
 
     await generateParams();
+    const url = `${import.meta.env.VITE_DOMAIN_URL}/users-admin/api/roles?${
+      params.value
+    }`;
 
-    return await fetch(
-      `${import.meta.env.VITE_DOMAIN_URL}/users-admin/api/roles?${
-        params.value
-      }`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then(async response => {
-        if (response.ok) {
-          const data = await response.json();
-          roles.value = data.records;
-          pageables.totalRecords = data.totalRecords;
-          pageables.totalPages = data.totalPages;
-          pageables.currentPage = data.currentPage + 1;
-          return;
-        }
-        await defineNotification({
-          message: await response.statusText,
-          error: true,
-        });
-      })
-      .catch(async err => {
-        await defineNotification({
-          message: err,
-          error: true,
-        });
-      })
-      .finally(() => (isLoading.value = false));
+    const { isFetching, error, data } = await useFetch(url).get().json();
+    isLoading.value = isFetching.value;
+    if (error.value) {
+      errors.value = error.value;
+      await defineNotification({
+        message: error.value,
+        error: true,
+      });
+    }
+    roles.value = data.value.records;
+    pageables.totalRecords = data.value.totalRecords;
+    pageables.totalPages = data.value.totalPages;
+    pageables.currentPage = data.value.currentPage + 1;
   }
 
   async function fetchUserRoles(userRefId: string) {
@@ -97,18 +82,6 @@ export const useRoles = () => {
       .finally(() => (isLoading.value = false));
   }
 
-  async function resetWebPassword(payload: {
-    username: string;
-    userRefId: string;
-    tenantId: string;
-  }) {
-    const url = `${
-      import.meta.env.VITE_DOMAIN_URL
-    }/users-admin/api/users/reset-password`;
-
-    return fetch(url, {});
-  }
-
   return {
     roles,
     isLoading,
@@ -117,6 +90,5 @@ export const useRoles = () => {
     userRoles,
     fetchRoles,
     fetchUserRoles,
-    resetWebPassword,
   };
 };
