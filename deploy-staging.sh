ff391dff542b2d911755b06d46d0d92db8be90ca
morgan4080 <morganmurungi@live.com>
2023-07-14 11:11:26 +0300

    remove vtscemit

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -5,10 +5,10 @@ IMAGE_ID=$(docker image ls | grep prestaadminui | awk '{ print $3 }')
 
 if [ "$CONTAINER_ID" ] && [ "$IMAGE_ID" ]; then
     docker stop "$CONTAINER_ID" &&
-     docker rmi "$IMAGE_ID" -f && git pull &&
+     docker rmi "$IMAGE_ID" -f && git pull && git checkout main &&
       docker build --build-arg env=staging -t prestaadminui/latest . &&
        docker run -d -p 4081:80 prestaadminui/latest
 else
-  git pull && docker build --build-arg env=staging -t prestaadminui/latest . &&
+  git pull && git checkout main && docker build --build-arg env=staging -t prestaadminui/latest . &&
          docker run -d -p 4081:80 prestaadminui/latest
 fi
