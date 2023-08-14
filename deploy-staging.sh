302953dfeb6b79bf65d03e490249a151c6677c4a
morgan4080 <morganmurungi@live.com>
2023-08-14 22:23:31 +0300

    deploy staging

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,12 +3,15 @@
 CONTAINER_ID=$(docker ps | grep prestaadminui | awk '{ print $1 }')
 IMAGE_ID=$(docker image ls | grep prestaadminui | awk '{ print $3 }')
 
+echo "$CONTAINER_ID"
+echo "$IMAGE_ID"
+
 if [ "$CONTAINER_ID" ] && [ "$IMAGE_ID" ]; then
     docker stop "$CONTAINER_ID" &&
      docker rmi "$IMAGE_ID" -f && git pull && git checkout main &&
       docker build --build-arg env=staging -t prestaadminui/latest . &&
        docker run -d -p 9099:80 prestaadminui/latest
 else
-  git pull && git checkout main && docker build --build-arg env=staging -t prestaadminui/latest . &&
+  git pull && git checkout development && docker build --build-arg env=staging -t prestaadminui/latest . &&
          docker run -d -p 9099:80 prestaadminui/latest
 fi
