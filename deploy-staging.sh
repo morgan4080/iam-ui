829470f6ee25cbe8a5bdef937e49231fd9bb30df
morgan4080 <morganmurungi@live.com>
2023-08-15 15:04:25 +0300

    change deployment script

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -8,7 +8,7 @@ echo "$IMAGE_ID"
 
 if [ "$CONTAINER_ID" ] && [ "$IMAGE_ID" ]; then
     docker stop "$CONTAINER_ID" &&
-     docker rmi "$IMAGE_ID" -f && git pull && git checkout main &&
+     docker rmi "$IMAGE_ID" -f && git pull && git checkout development &&
       docker build --build-arg env=staging -t prestaadminui/latest . &&
        docker run -d -p 9099:80 prestaadminui/latest
 else
